7f37b4060789cbafe4205157e07601ed8d4b5b18
Norvin <ngydev@outlook.com>
2016-08-04 22:09:58 +0800

    Remove unnecessary comments from JobController

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'role', 'company'
+        'name', 'email', 'role', 'company'
     ];
 
     /**
